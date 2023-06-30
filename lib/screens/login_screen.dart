67203b26a64fb834687e03ebc5b81ec04a81a73b
Piyush <91675982+PiyushKBhattacharyya@users.noreply.github.com>
2023-06-30 12:20:59 +0530

    Commit:30-06-23,12:20

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -60,6 +60,68 @@ class _LoginScreenState extends State<LoginScreen> {
                           : Icon(CupertinoIcons.eye_fill),
                     )),
               ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Material(
+                color: Color(0xFF7165D6),
+                borderRadius: BorderRadius.circular(10),
+                child: InkWell(
+                  onTap: () {
+                    // Navigator.push(context,
+                    //     MaterialPageRoute(builder: (context) => LoginScreen()));
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                    child: Center(
+                      child: Text(
+                        "Log In",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't Have an Account?",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black54,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                     MaterialPageRoute(
+                      builder: (context) => SignUpScreen()) 
+                    )
+                  },
+                  child: Text(
+                    'Create Account',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF7165D6),
+                    ),
+                  ),
+                ),
+              ],
             )
           ],
         )),
